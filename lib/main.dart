bc9b5ef682e26ce5757e3f90241381e52f621874
Ankit Kumar Singh <ankitkumarsoam@gmail.com>
2021-05-22 21:06:03 +0530

    my first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'trippin',
+      home: Scaffold(
+        body:Center(
+            child: Text('Team trippin', textDirection: TextDirection.ltr, style: TextStyle(fontSize: 40 , ),),
+
+        )
+      )
+    )
+  );
+}
+
+
+
+   
